5a70ab30cfbd12c1bc8da3cbb017d9ef93d3a4a1
jongbin-choi <kooma1123@naver.com>
2022-04-24 10:16:22 +0900

    객체지향 문제풀기

--- src/com/javaex/ex06/CConverter.java
+++ src/com/javaex/ex06/CConverter.java
@@ -1,6 +1,14 @@
 package com.javaex.ex06;
 
 public class CConverter {
+	
+	
+	
+    // static 메소드는 인스턴스를 생성하지 않아도 static 메소드를 사용할 수 있다. 
+	// ex) CConverter a = new CConverter(); <- new 연산자를 이용한 것을 말함
+	// static 메소드는 static을 이용한 메소드들끼리 변수의 값을 공유할 수 있다.
+	// 그래서 CConverterApp에서 new연산자 없이 바로 CConverter.toDoller(10000000)입력하면
+	// toDoller의 메소드가 실행될 수 있는 것이다.
     
     public static double rate;
     
@@ -17,4 +25,7 @@ public class CConverter {
     }
     
 
+    
+    
+    
 }
